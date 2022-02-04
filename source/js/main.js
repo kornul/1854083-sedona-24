d9aaacc11426a230a527d6873aacc5a6e5252c40
Евгений <chebyrechka@mail.ru>
2022-02-04 10:21:43 +0300

    Защита 1.1

--- source/js/main.js
+++ source/js/main.js
@@ -1,8 +1,10 @@
 var navMain = document.querySelector('.main-nav');
       var navToggle = document.querySelector('.main-nav__toggle');
+      var pageHeader = document.querySelector('.page-header');
 
       navMain.classList.add('main-nav--closed');
-      navMain.classList.remove('main-nav--opened');
+      navMain.classList.remove('main-nav--nojs');
+      pageHeader.classList.remove('page-header--nojs');
 
       navToggle.addEventListener('click', function () {
         if (navMain.classList.contains('main-nav--closed')) {
